a8aee834a4a50401f4180b836257aeecc01852b1
ryuji-saitou <rs0623idmanagement@gmail.com>
2021-06-11 14:13:31 +0000

    [finish] 4-9

--- meshiterro/app/models/post_image.rb
+++ meshiterro/app/models/post_image.rb
@@ -1,4 +1,14 @@
 class PostImage < ApplicationRecord
   belongs_to :user
   attachment :image #カラム名はimage_idだが "_id" は不要
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  
+  validates:shop_name, presence:true
+  validates:image, presence:true
+  
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
+
 end
